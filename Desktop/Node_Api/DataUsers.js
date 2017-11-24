bbabf1a6f97215d1207278b2e7b2b23d76c721ed
samveltx <samveltx@gmail.com>
2017-11-24 13:59:33 +0400

    add

---
+++ Desktop/Node_Api/DataUsers.js
@@ -0,0 +1,43 @@
+const data = [
+  {
+    "id": 1,
+    "first_name": "Saqo",
+    "last_name": "Asoyan",
+    "email": "Saqo@gmail.com",
+    "website": "http://sweb.com"
+  },
+
+  {
+    "id": 2,
+    "first_name": "Karen",
+    "last_name": "Karenyan",
+    "email": "Karen@gmail.com",
+    "website": "http://scotch.am"
+  },
+
+  {
+    "id": 3,
+    "first_name": "Nick",
+    "last_name": "Ceryan",
+    "email": "Nick@gmail.com",
+    "website": "http://nick.com"
+  },
+
+  {
+    "id": 4,
+    "first_name": "Ado",
+    "last_name": "Kukyan",
+    "email": "ado@gmail.com",
+    "website": "http://ado.com"
+  },
+
+  {
+    "id": 5,
+    "first_name": "Sona",
+    "last_name": "Llyan",
+    "email": "So_1_@gmail.com",
+    "website": "http://Son.com"
+  }
+
+];
+module.exports = data;
